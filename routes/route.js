537dddfe4280b5efbcd7bf6571528cb00046ed11
Kailey Colestock <kaileycolestock@Kaileys-MacBook-Pro.local>
2016-02-19 14:59:59 -0600

    Removed excess code

--- routes/route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var randomAnimal = express.Router();
-
-randomAnimal.get('/', function(req, res) {
-    function randomNumber(min, max){ return Math.floor(Math.random() * (1 + max - min) + min); }
-    res.send(randomNumber(1, 100));
-});
-
-module.exports = randomAnimal;
